363d12dfcd09644712d49177e96eabd871110fa8
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-11-03 18:49:45 +0000

    Fixed stylesheet relative path

--- templates/tpl_common.php
+++ templates/tpl_common.php
@@ -1,16 +1,19 @@
-<?php function draw_header($username){
+<?php 
+
 /**
  * Draws the header for all pages. Receives an username
  * if the user is logged in in order to draw the logout
  * link.
- */ ?>
+ */
+function draw_header($username){
+?>
 <!DOCTYPE html>
 <html lang="en-US">
 <html>
 
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
@@ -31,17 +34,18 @@
                 </nav>
                 <?php }?>
     </header>
-    <?php } ?>
+<?php } ?>
 
-    <?php function draw_footer() { 
+
+<?php 
 /**
  * Draws the footer for all pages.
- */ ?>
-    <footer>
-        <p>(c)MIEIC - LTW 2018/2019 T3G05</p>
-        <p>Luis Oliveira; Ricardo Silva; Fábio Gaspar</p>
-    </footer>
-</body>
-
+ */
+function draw_footer() { ?>
+        <footer>
+            <p>(c)MIEIC - LTW 2018/2019 T3G05</p>
+            <p>Luis Oliveira; Ricardo Silva; Fábio Gaspar</p>
+        </footer>
+    </body>
 </html>
 <?php } ?>
