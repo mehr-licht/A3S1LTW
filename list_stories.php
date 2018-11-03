21bea6f6d548dc90008de3086e9c943f38a5223d
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-11-03 18:43:06 +0000

    Updated stories page to use template functions

---
+++ list_stories.php
@@ -0,0 +1,18 @@
+<?php
+    include_once('templates/tpl_common.php');
+    draw_header(null); // TODO
+?>
+    <section id="stories">
+        <article>
+            <p>exper</p>
+        </article>
+
+    </section>
+
+    <aside>
+        <p>aside</p>
+    </aside>
+
+<?php
+    draw_footer();
+?>
