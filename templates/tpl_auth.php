10e33364575ee76fc98a749f870dc0d04481cd66
Mehr Licht! <mehrlicht@gmail.com>
2018-11-07 11:47:51 +0000

    stabilized login and signup

---
+++ templates/tpl_auth.php
@@ -0,0 +1,55 @@
+<?php function draw_login() { 
+/**
+ * Draws the login section.
+ */ ?>
+  <div id="login">
+
+<form class="form" action="login.php" method="post">
+    <div id="userinput" class="inputs">
+        <label class="labeluser">username: </label>
+        <input id="loginUsername" type="text" placeholder="username" required="required" />
+    </div>
+    <div id="pwdinput" class="inputs">
+        <label class="labelpwd">password:</label>
+        <input id="loginPwd" type="password" placeholder="password" />
+    </div>
+    <!-- <input id="submitButton" type="submit" value="login"> -->
+    <div id="buttons">
+        <label class="LblBtn"></label>
+        <button class="button" formaction="login.php" formmethod="post">login</button>
+        <label id="please">if you still have no account please register first</label><button class="button" onclick="location.href='signup.html'" type="button">
+    signup</button>
+    </div>
+</form>
+
+
+</div>
+<?php } ?>
+
+<?php function draw_signup() { 
+/**
+ * Draws the signup section.
+ */ ?>
+   <div id="signup">
+        <form class="form" action="signup.php" method="post">
+            <div id="userinput" class="inputs">
+                <label class="label1">desired username: </label>
+                <input id="SignupUsername" type="text" placeholder="desired username" required="required" />
+            </div>
+            <div id="pwdinput" class="inputs">
+                <label class="label2">password: </label>
+                <input id="SignupPwd" type="password" placeholder="password" />
+            </div>
+            <div id="repwdinput" class="inputs">
+                <label class="label3"> repeat password: </label>
+                <input id="SignupRepeatPwd" type="password" placeholder="password" />
+            </div>
+            <div id="buttons">
+                <!-- <input id="submitButton" type="submit" value="login"> -->
+                <label id="LblBtn"> </label>
+                <button class="button" formaction="signup.php" formmethod="post">signup</button>
+                <label id="please">already have an account? please login</label><button class="button" onclick="location.href='login.html'" type="button">login</button>
+            </div>
+        </form>
+    </div>
+<?php } ?>
--- templates/tpl_common.php
+++ templates/tpl_common.php
@@ -1,32 +1,24 @@
-<<<<<<< HEAD
-<?php function draw_header($username) { 
+<?php function draw_header($username){
 /**
  * Draws the header for all pages. Receives an username
  * if the user is logged in in order to draw the logout
  * link.
- */?>
-  <!DOCTYPE html>
+ */ ?>
+<!DOCTYPE html>
 <html lang="en-US">
 <html>
 
 <head>
-    <title>signup - Yet Another Site - T1G05 - LTW 2018/2019 - MIEIC</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../css/style.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
     <header>
         <p>Yet Another Site</p>
-<<<<<<< HEAD
-        <a id="loginhref" href="../pages/login.php">Login</a>
-        <a id="signuphref" href="../pages/signup.php">Signup</a>
-        <p>Logout</p>
-=======
         <?php if ($username == NULL) { ?>
-        <a id="loginhref" href="login.html">Login</a>
-            <a id="signuphref" href="signup.html">Signup</a>
+        <a id="loginl" href="../pages/login.php">Login</p>
+            <a id="signupl" href="../pages/signup.php">Signup</p>
                 <?php }?>
                 <?php if ($username != NULL) { ?>
                 <nav>
@@ -38,66 +30,18 @@
                     </ul>
                 </nav>
                 <?php }?>
->>>>>>> 19f74840529628105fd9e9356d5fb7d78df3c60a
     </header>
     <?php } ?>
 
-=======
-<?php function draw_login() { 
-/**
- * Draws the login section.
- */ ?>
-  <section id="login">
-    
-    <header><h2>Welcome Back</h2></header>
-
-    <form method="post" action="../actions/action_login.php">
-      <input type="text" name="username" placeholder="username" required>
-      <input type="password" name="password" placeholder="password" required>
-      <input type="submit" value="Login">
-    </form>
->>>>>>> master
-
-    <footer>
-      <p>Don't have an account? <a href="signup.php">Signup!</a></p>
-    </footer>
-
-  </section>
-<?php } ?>
-
-<<<<<<< HEAD
-
-<?php function draw_footer() { 
+    <?php function draw_footer() { 
 /**
  * Draws the footer for all pages.
  */ ?>
- <footer>
+    <footer>
         <p>(c)MIEIC - LTW 2018/2019 T3G05</p>
-        <p>Fábio Gaspar; Luis Oliveira; Ricardo Silva</p>
+        <p>Luis Oliveira; Ricardo Silva; Fábio Gaspar</p>
     </footer>
 </body>
 
 </html>
 <?php } ?>
-=======
-<?php function draw_signup() { 
-/**
- * Draws the signup section.
- */ ?>
-  <section id="signup">
-
-    <header><h2>New Account</h2></header>
-
-    <form method="post" action="../actions/action_signup.php">
-      <input type="text" name="username" placeholder="username" required>
-      <input type="password" name="password" placeholder="password" required>
-      <input type="submit" value="Signup">
-    </form>
-
-    <footer>
-      <p>Already have an account? <a href="login.php">Login!</a></p>
-    </footer>
-
-  </section>
-<?php } ?>
->>>>>>> master
