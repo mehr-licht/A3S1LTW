cebd4b11c5c8931e89777017bc6e03dcc5f61735
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-12-05 19:52:30 +0000

    Attempt to fix

--- pages/postView.php
+++ pages/postView.php
@@ -4,34 +4,33 @@
     draw_header(NULL);
     if(isset($_GET['postId'])) {
         $post = getPostByID($_GET['postId']);
-        print_r($post);
-    ?> 
+        print_r($post);?> 
         <section>
-            <h1><?$post[titulo]?></h1>
-            <p>By <?$post[iduser]?> | <?$post[data]?></p>
-            <p><?$post[conteudo]?></p>
+            <h1><?=$post[titulo]?></h1>
+            <p>By <?=$post[iduser]?> | <?=$post[data]?></p>
+            <p><?=$post[conteudo]?></p>
             <div>
-                <span>Up: <?$post[votesUp]?></span>
-                <span>Down: <?$post[votesDown]?></span>
+                <span>Up: <?=$post[votesUp]?></span>
+                <span>Down: <?=$post[votesDown]?></span>
             </div>
         </section>
 
         <section>
             <h2>Comments</h2>
-            <?
+            <?=
             $comments = getComments($_GET['postId']);
             foreach ($comments as $comment) {?>
                 <article>
                     <header>
-                        <span><?$comment[iduser]?></span>
-                        <span><?$comment[data]?></span>
+                        <span><?=$comment[iduser]?></span>
+                        <span><?=$comment[data]?></span>
                     </header>
-                    <p><?$comment[comentConteudo]?></p>
+                    <p><?=$comment[comentConteudo]?></p>
                     <footer>
                         <span>Votes (soon)</span>
                         <span>Reply (soon)</span>
                     </footer>
                 </article>
-            <?}?>
+            <?php}?>
         </section>
-    <?}?>  
+    <?php}?>  
