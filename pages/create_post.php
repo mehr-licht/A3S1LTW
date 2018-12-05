8447d719ceb1edb6c341ff61186a7fa4d2466e29
Ricardo MG Silva <up201607780@fe.up.pt>
2018-12-05 01:53:31 +0000

    create_post.php similar to 4chan

--- pages/create_post.php
+++ pages/create_post.php
@@ -1,6 +1,7 @@
 <?php 
 include_once('../includes/session.php');
 include_once('../templates/tpl_common.php');
+include_once('../database/db_list.php');
 
 if (!isset($_SESSION['username'])){
     die(header('Location: ../pages/login.php'));
@@ -36,6 +37,30 @@ if( $_SESSION['signup'][0]['type'] == 'error'){
         
     </form>
 </div>
+
+
+
+<link href="../css/list_news.css" type="text/css" rel="stylesheet">
+    <section id="stories">
+        <?php 
+            // ou podemos subsituir a funcao em baixo por getAllPostsUSER($_SESSION['username'] 
+            foreach(getAllPostsOrderByDate() as $post) {
+                echo 
+                "<article --data-postid=\"$post[idPost]\">
+                    <h1>$post[titulo]</h1>
+                    <div class=\"post_content\">
+                        <div class=\"post_votes\">
+                            <div>Up: $post[votesUp]</div>
+                            <div>Down: $post[votesDown]</div>
+                        </div>
+                        <img alt=\"Some random image\" src=\"https://picsum.photos/200\">
+                        <p>$post[conteudo]</p>
+                    </div>
+                    <a href=\"postView.php?postId=$post[idPost]\">Read More</a>
+                </article>";
+            }
+        ?>
+    </section>
 
 
 
