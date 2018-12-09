e753c3ac3b172c94f3f9182ead66df6c3e49c5dd
Fabio Gaspar <fabiodanielreis@hotmail.com>
2018-12-09 15:05:11 +0000

    Performed some code cleanup

--- js/post.js
+++ js/post.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function userVoted(username, postId, vote) {
+function api_user_voted_post(postId, vote) {
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.open('POST', '/api/post_vote.php');
     xmlhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
@@ -9,10 +9,6 @@ function userVoted(username, postId, vote) {
         vote: vote,
         username: 'fabioD'
     }));
-}
-
-function voteClickHandler(articleNode, voteType) {
-
 }
 
 function upvoteClickHandler(articleNode) {
@@ -25,37 +21,42 @@ function upvoteClickHandler(articleNode) {
     let isUpVoted = (upvoteBtnClass.indexOf('active') == -1) ? false : true; // flag telling if user has voted up
     let isDownVoted = (downvoteBtnClass.indexOf('active') == -1) ? false : true; // flag telling if user has voted down
 
+    let newVote; // the new vote value to insert on the database
     if(isDownVoted) {
-        upvoteBtnClass += " active";
-        $upvoteBtn.setAttribute('class', upvoteBtnClass);
-        // remove 'active' class from downvote btn
+        /**
+         * The user has downvoted previously, but it's upvoting now
+         * The vote becomes +1 instead of -1
+         */
+        // set upvote button as active
+        $upvoteBtn.setAttribute('class', upvoteBtnClass + 'active');
+        // removes the active state from downvote
         $downvoteBtn.setAttribute('class', downvoteBtnClass.replace('active', ''));
-        // update votes value
-        $votesCounter.innerText = Number($votesCounter.innerText) + 2; // remove the current -1, add 1, eq. to adding 2
-        // perform API call
-        userVoted('fabioD', postId, 1);
-        // update flags
-        isUpVoted = true;
-        isDownVoted = false;
+        // update the votes counter (+2, +1 for reversing the downvote, +1 for new positive vote)
+        $votesCounter.innerText = Number($votesCounter.innerText) + 2;
+        // 
+        newVote = 1;
     } else if (isUpVoted) {
-        // if user already voted Up, but it's clicking the upvote button, then remove the vote
+        /**
+         * The user has upvoted previously, but it's pressing the upvote button again
+         * Thus, the user pretends to undo it's vote
+         */
+        // remove the active state from upvote button
         $upvoteBtn.setAttribute('class', upvoteBtnClass.replace('active', ''));
-        // update votes value
+        // update the votes counter
         $votesCounter.innerText = Number($votesCounter.innerText) - 1;
-        // perform API call
-        userVoted('fabioD', postId, 0);
-        // update flag
-        isUpVoted = false;
+
+        newVote = 0;
     } else {
         // user hasn't voted yet
         $upvoteBtn.setAttribute('class', upvoteBtnClass + ' active');
         // update votes value
         $votesCounter.innerText = Number($votesCounter.innerText) + 1;
         // perform API call
-        userVoted('fabioD', postId, 1);
-        // update flag
-        isUpVoted = true;
+        newVote = 1;
     }
+
+    // perform API call
+    api_user_voted_post(postId, newVote);
 }
 
 function downvoteClickHandler(articleNode) {
@@ -68,34 +69,49 @@ function downvoteClickHandler(articleNode) {
     let isUpVoted = (upvoteBtnClass.indexOf('active') == -1) ? false : true; // flag telling if user has voted up
     let isDownVoted = (downvoteBtnClass.indexOf('active') == -1) ? false : true; // flag telling if user has voted down
 
+    let newVote; // the new vote value to insert on the database
+
     if(isDownVoted) {
-        // if user already voted Down, but it's clicking the downvote button, then remove the vote
+        /* 
+         * the user previously downvoted and pressed the down vote button
+         * thus the user doesn't have any vote anymore
+         */
+        // remove the active state from the downvote button
         $downvoteBtn.setAttribute('class', downvoteBtnClass.replace('active', ''));
-        // update votes value
+        // update votes value (by incrementing 1)
         $votesCounter.innerText = Number($votesCounter.innerText) + 1;
-        // perform API call
-        userVoted('fabioD', postId, 0);
+        // 
+        newVote = 0;
     } else if (isUpVoted) {
-        // if user already voted Up, but it's clicking the downvote button...
+        /**
+         * The user previously upvoted, but now is pressing the down vote
+         * It's vote becomes -1 instead of +1
+         */
+        // remove active state from upvote button
         $upvoteBtn.setAttribute('class', upvoteBtnClass.replace('active', ''));
+        // sets downvote button as active
+        $downvoteBtn.setAttribute('class', downvoteBtnClass + ' active');
         // update votes value
         $votesCounter.innerText = Number($votesCounter.innerText) - 2;
-        // activate downvote button
-        $downvoteBtn.setAttribute('class', downvoteBtnClass + ' active');
         // perform API call
-        userVoted('fabioD', postId, -1);
+        newVote = -1;
     } else {
-        // user hasn't voted yet
+        /**
+         * The user hasn't voted yet
+         */
+        // Set downvote as active
         $downvoteBtn.setAttribute('class', downvoteBtnClass + ' active');
         // update votes value
         $votesCounter.innerText = Number($votesCounter.innerText) - 1;
         // perform API call
-        userVoted('fabioD', postId, -1);
+        newVote = -1;
     }
+
+    api_user_voted_post(postId, newVote);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-
+    // Add click event listeners on all vote buttons
     let allArticles = document.querySelectorAll("#stories > article");
 
     for(let articleNode of allArticles) {
