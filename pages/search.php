c464cd20eb2a087e2aedb80af2dec8d1aa4a17d6
Mehr Licht! <mehrlicht@gmail.com>
2018-12-17 16:49:41 +0000

    corrected footer in between search results (but it comes up...)

--- pages/search.php
+++ pages/search.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="../css/auth.css">
+
 <?php
 include_once '../includes/session.php';
 include_once '../database/db_user.php';
@@ -21,131 +21,147 @@ if (checkTimeout() || !isset($_SESSION['username'])){
 
 regenerateSession();
 
-?>
-<body><?php
 draw_header($_SESSION['username']);
 
-switch ($_GET['choice']) {
-    case 'users': ?>
-<!-- list of Users-->
-<section id="users">
-  <div class="users">
-    <?php $users = searchUser($_GET['search']); 
-    if (count($users)) { ?> <h1>Users</h1> 
-    <?php  foreach ($users as $user) { ?>
-    <article class="userSearch">
-      <div class="<?= $user['username'] ?>">
-        <a href="../pages/profile.php?user=<?= $user['username'] ?>">
-          <?= $user['username']; ?></a>
-      </div>
-    </article>
-    <?php 
-  }  } ?>
-  </div>
-</section>
-<?php
-                break;
-            case 'comments': ?>
-<!-- list of Comments-->
-<section id="comments">
-  <div class="comments">
-    <?php $allComments = searchComents($_GET['search']); ?>
-    <?php if(count($allComments)) { ?> <h1>Comments:</h1> 
-    <?php foreach ($allComments as $comment) { ?>
-    <article class="commentSearch">
-      <div class="<?= $comment['idComent'] ?>">
-        <a href="../pages/postView.php?postId=<?= $comment['idPost'] ?>#<?= $comment['idComent'] ?>">
-          <?= $comment['idComent']; ?> :
-          <?= substr($comment['comentContent'], 0, 50) ?></a>• 
-        <?= $comment['data'] ?>
-        <!-- plus owner of post? -->
-      </div>
-    </article>
-    <?php 
-   } } ?>
-  </div>
-</section>
-<?php
-                break;
-            case 'posts': ?>
-<!-- list of Posts-->
-<section id="posts">
-  <div class="posts">
-    <?php $allPosts = searchPosts($_GET['search']); 
-    if(count($allPosts)) { ?> <h1>Posts</h1>
-    <?php foreach ($allPosts as $post) { ?>
-    <article class="postSearch">
-      <div class="<?= $post['title'] ?>">
-        <a href="../pages/postView.php?postId=<?= $post['idPost'] ?>">
-          <?= $post['title'] ?> </a>
-        <?= $post['date'] ?>
-        <!-- plus votes? -->
-      </div>
-    </article>
-    <?php 
-          }  } ?>
-  </div>
-</section>
-<?php
-                        break;
-                    case 'any': ?>
-<section id="users">
-  <div class="users">
-    <?php $users = searchUser($_GET['search']); 
-              if(count($users)) { ?> <h1>Users</h1>
-             <?php foreach ($users as $user) { ?>
-    <article class="userSearch">
-      <div class="<?= $user['username'] ?>">
-        <a href="../pages/profile.php?user=<?= $user['username'] ?>">
-          <?= $user['username']; ?></a>
-      </div>
-    </article>
-    <?php 
-          }  } ?>
-  </div>
-</section>
-<section id="comments">
-    <div class="comments">
-    <?php $allComments = searchComments($_GET['search']); 
-if(count($allComments)) { ?> <h1>Comments</h1>
-    <?php foreach ($allComments as $comment) { ?>
-    <article class="commentSearch">
-      <div class="<?= $comment['idComent'] ?>">
-        <a href="../pages/postView.php?postId=<?= $comment['idPost'] ?>#<?= $comment['idComent'] ?>">
-          <?= substr($comment['comentContent'], 0, 50) ?></a>
-        <?= $comment['data'] ?>
-        <!-- plus owner of post? -->
-      </div>
-    </article>
-    <?php 
-    } } ?>
-  </div>
-</section>
-<!-- list of Posts-->
-<section id="posts">
-  <div class="posts">
-    <?php $allPosts = searchPosts($_GET['search']); 
-    if(count($allPosts)) { ?> <h1>Posts</h1>
-    <?php foreach ($allPosts as $post) { ?>
-    <article class="postSearch">
-      <div class="<?= $post['title'] ?>">
-        <a href="../pages/postView.php?postId=<?= $post['idPost'] ?>">
-          <?= $post['title'] ?> </a>
-        <?= $post['date'] ?>
-        <!-- plus votes? -->
-      </div>
-    </article>
-    <?php 
-        } } ?>
-  </div>
-</section>
-<?php
-                    break;
-                default:
-                    break;
-            }
+draw_results($_GET['choice'],$_GET['search']);
+
+ /**
+         * Draws the results of the search query.
+         * @param choice type of choice: any,users,posts,comments
+         * @param search pattern to search
+         */
+        function draw_results($choice,$search){
+          switch ($choice) {
+              case 'users': ?>
+          <!-- list of Users-->
+          <body>
+          <section id="users">
+              <div class="users">
+                  <?php $users = searchUser($search); 
+              if (count($users)) { ?>
+                  <h1>Users</h1>
+                  <?php  foreach ($users as $user) { ?>
+                  <article class="userSearch">
+                      <div class="<?= $user['username'] ?>">
+                          <a href="../pages/profile.php?user=<?= $user['username'] ?>">
+                              <?= $user['username']; ?></a>
+                      </div>
+                  </article>
+                  <?php 
+            }  } ?>
+              </div>
+          </section>
+          <?php
+                          break;
+                      case 'comments': ?>
+          <!-- list of Comments-->
+          <section id="comments">
+              <div class="comments">
+                  <?php $allComments = searchComents($search); ?>
+                  <?php if(count($allComments)) { ?>
+                  <h1>Comments:</h1>
+                  <?php foreach ($allComments as $comment) { ?>
+                  <article class="commentSearch">
+                      <div class="<?= $comment['idComent'] ?>">
+                          <a href="../pages/postView.php?postId=<?= $comment['idPost'] ?>#<?= $comment['idComent'] ?>">
+                              <?= $comment['idComent']; ?> :
+                              <?= substr($comment['comentContent'], 0, 50) ?></a>•
+                          <?= $comment['data'] ?>
+                          <!-- plus owner of post? -->
+                      </div>
+                  </article>
+                  <?php 
+             } } ?>
+              </div>
+          </section>
+          <?php
+                          break;
+                      case 'posts': ?>
+          <!-- list of Posts-->
+          <section id="posts">
+              <div class="posts">
+                  <?php $allPosts = searchPosts($search); 
+              if(count($allPosts)) { ?>
+                  <h1>Posts</h1>
+                  <?php foreach ($allPosts as $post) { ?>
+                  <article class="postSearch">
+                      <div class="<?= $post['title'] ?>">
+                          <a href="../pages/postView.php?postId=<?= $post['idPost'] ?>">
+                              <?= $post['title'] ?> </a>
+                          <?= $post['date'] ?>
+                          <!-- plus votes? -->
+                      </div>
+                  </article>
+                  <?php 
+                    }  } ?>
+              </div>
+          </section>
+          <?php
+                                  break;
+                              case 'any': ?>
+          <section id="users">
+              <div class="users">
+                  <?php $users = searchUser($search); 
+                        if(count($users)) { ?>
+                  <h1>Users</h1>
+                  <?php foreach ($users as $user) { ?>
+                  <article class="userSearch">
+                      <div class="<?= $user['username'] ?>">
+                          <a href="../pages/profile.php?user=<?= $user['username'] ?>">
+                              <?= $user['username']; ?></a>
+                      </div>
+                  </article>
+                  <?php 
+                    }  } ?>
+              </div>
+          </section>
+          <section id="comments">
+              <div class="comments">
+                  <?php $allComments = searchComments($search); 
+          if(count($allComments)) { ?>
+                  <h1>Comments</h1>
+                  <?php foreach ($allComments as $comment) { ?>
+                  <article class="commentSearch">
+                      <div class="<?= $comment['idComent'] ?>">
+                          <a href="../pages/postView.php?postId=<?= $comment['idPost'] ?>#<?= $comment['idComent'] ?>">
+                              <?= substr($comment['comentContent'], 0, 50) ?></a>
+                          <?= $comment['data'] ?>
+                          <!-- plus owner of post? -->
+                      </div>
+                  </article>
+                  <?php 
+              } } ?>
+              </div>
+          </section>
+          <!-- list of Posts-->
+          <section id="posts">
+              <div class="posts">
+                  <?php $allPosts = searchPosts($search); 
+              if(count($allPosts)) { ?>
+                  <h1>Posts</h1>
+                  <?php foreach ($allPosts as $post) { ?>
+                  <article class="postSearch">
+                      <div class="<?= $post['title'] ?>">
+                          <a href="../pages/postView.php?postId=<?= $post['idPost'] ?>">
+                              <?= $post['title'] ?> </a>
+                          <?= $post['date'] ?>
+                          <!-- plus votes? -->
+                      </div>
+                  </article>
+                  <?php 
+                  } } ?>
+              </div>
+          </section>
+                </body>
+          <?php
+                              break;
+                          default:
+                              break;
+                      }
+                  }
+      
+      
 
 draw_footer();
-?>
-</body><?php
+
 ?>
